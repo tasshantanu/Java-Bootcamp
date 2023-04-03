1f395de5a316de4f200c3e17eafde7bf85244de3
Tasfique A Shantanu <90642909+tasshantanu@users.noreply.github.com>
2023-04-03 12:57:21 -0400

    Update FirstProgram.java

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,4 +1,13 @@
 // I'm a comment. I have no impact on your code.
 
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Printing is Fun!");
+        System.out.println("Java > Python");
+        System.out.println("I spilled Java all over my paper.");
+        System.out.println("My dog ate my Java");
+
+    }
+}
 
 // Instructions for this workbook are on Learn the Part (See the Udemy Video: Workbook 1.1 to access the link).
