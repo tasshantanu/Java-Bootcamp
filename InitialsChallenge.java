d00d5cfddf68453fc9bc6b34cb3f04d9e83748a3
Tasfique A Shantanu <90642909+tasshantanu@users.noreply.github.com>
2023-04-03 16:12:16 -0400

    Add files via upload
    Initials Challenge V1

---
+++ InitialsChallenge.java
@@ -0,0 +1,14 @@
+public class InitialsChallenge {
+    public static void main(String[] args) {
+        System.out.println("T T T T T" + "  " + "  S S S  ");
+        System.out.println("    T    " + "  " + "S       S");
+        System.out.println("    T    " + "  " + "S        ");
+        System.out.println("    T    " + "  " + "  S S S    ");
+        System.out.println("    T    " + "  " + "        S");
+        System.out.println("    T    " + "  " + "S       S");
+        System.out.println("    T    " + "  " + "  S S S  ");
+
+
+    }
+    
+}
